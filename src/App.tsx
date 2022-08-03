271acec152c4852bbba37eacf66988d1d13042db
nicocappabianca <nicolas.cappabianca@gmail.com>
2022-08-03 15:54:17 -0300

    fix(Home): move service declaration to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import { useState, useEffect, useRef } from "react";
 import { Home } from "./pages";
 import styles from "./App.module.scss";
+import { useJsApiLoader } from "@react-google-maps/api";
 
-function App() {
+const App = () => {
+  const [service, setService] = useState<google.maps.places.PlacesService>();
+  const fakeMapRef = useRef<HTMLDivElement>(null);
+
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY as string,
+    libraries: ["places"],
+  });
+
+  useEffect(() => {
+    if (isLoaded && fakeMapRef.current)
+      setService(new google.maps.places.PlacesService(fakeMapRef.current));
+  }, [isLoaded]);
+
   return (
     <div className={styles.App}>
-      <Home />
+      {service && <Home mapsService={service} />}
+      <div className={styles["fake-map"]} ref={fakeMapRef}></div>
     </div>
   );
-}
+};
 
 export default App;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,35 +1,24 @@
-import React, { useEffect, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import styles from "./Home.module.scss";
-import { useJsApiLoader } from "@react-google-maps/api";
 
 let searchTimeout: NodeJS.Timeout;
 
-const Home = () => {
-  const [service, setService] = useState<google.maps.places.PlacesService>();
+interface HomeProps {
+  mapsService: google.maps.places.PlacesService;
+}
+
+const Home: FC<HomeProps> = ({ mapsService }) => {
   const [inputValue, setInputValue] = useState("");
-
-  const { isLoaded } = useJsApiLoader({
-    id: "google-map-script",
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY as string,
-    libraries: ["places"],
-  });
-
-  useEffect(() => {
-    if (isLoaded)
-      setService(
-        new google.maps.places.PlacesService(document.createElement("div"))
-      );
-  }, [isLoaded]);
 
   useEffect(() => {
     clearTimeout(searchTimeout);
     searchTimeout = setTimeout(() => {
-      service?.findPlaceFromQuery(
+      mapsService?.findPlaceFromQuery(
         { query: inputValue, fields: ["ALL"] },
         (data) => console.log(data?.[0].geometry?.location?.lat())
       );
     }, 700);
-  }, [inputValue, service]);
+  }, [inputValue, mapsService]);
 
   return (
     <div className={styles.Home}>
