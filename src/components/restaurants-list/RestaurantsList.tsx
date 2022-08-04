d8586b3e5323289d7d575e7fc46d883fe897dcee
nicocappabianca <nicolas.cappabianca@gmail.com>
2022-08-04 12:03:00 -0300

    stytles(RestaurantsList)

--- src/components/restaurants-list/RestaurantsList.tsx
+++ src/components/restaurants-list/RestaurantsList.tsx
@@ -5,16 +5,29 @@ interface RestaurantsListProps {
   restaurants: google.maps.places.PlaceResult[];
 }
 
+const restaurantStatus = {
+  CLOSED_PERMANENTLY: "This restaurant is closed permanently.",
+  CLOSED_TEMPORARILY: "This restaurant is closed temporarily.",
+  OPERATIONAL: "This restaurant is open",
+};
+
 const RestaurantsList: FC<RestaurantsListProps> = ({ restaurants }) => {
   console.log(restaurants);
   return (
     <ul className={styles.RestaurantsList}>
       {restaurants
         .sort((a, b) => Number(b.rating) - Number(a.rating))
+        .slice(0, 10)
         .map((restaurant) => (
           <li key={restaurant.place_id}>
-            <h4>{restaurant.name}</h4>
-            <p>{restaurant.rating}</p>
+            <h4>
+              {Number(restaurant?.name?.length) > 40
+                ? `${restaurant.name?.substring(0, 35)}...`
+                : restaurant.name}
+            </h4>
+            {restaurant.business_status && (
+              <p>{restaurantStatus[restaurant.business_status]}</p>
+            )}
           </li>
         ))}
     </ul>
